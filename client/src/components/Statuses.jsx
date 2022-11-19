f603bf3b942520a492e1904d16266d0d0b7094d9
Jorge Macbook <jorgemartinez1990@gmail.com>
2022-11-19 10:45:16 -0500

    mediaQuery for 'Statuses' component added

--- client/src/components/Statuses.jsx
+++ client/src/components/Statuses.jsx
@@ -4,17 +4,25 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Unstable_Grid2';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import PaidIcon from '@mui/icons-material/Paid';
+import TaskIcon from '@mui/icons-material/Task';
+import HardwareIcon from '@mui/icons-material/Hardware';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
 
 const Statuses = () => {
+    
+    const matches = useMediaQuery('(min-width:600px)')
+    
 
   return (
     <Grid display="grid" gridTemplateColumns="repeat(4, 1fr)" gap={1}>
         <Grid gridColumn="span 1">
         <Card sx={{ minWidth:50, maxHeight: 90 }}>
-        <CardContent>
-        <Typography sx={{ fontSize: 10, textAlign: 'center' }} color="text.secondary" gutterBottom>
+        <CardContent sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+        {matches? <Typography sx={{ fontSize: 10, textAlign: 'center' }} color="text.secondary" gutterBottom>
             SALES
-        </Typography>
+        </Typography>:<LocalOfferIcon sx={{color: 'blue'}}/>}
         <Typography sx={{textAlign: 'center' }} color="text.secondary">
             4
         </Typography>
@@ -25,10 +33,10 @@ const Statuses = () => {
   </Grid>
   <Grid gridColumn="span 1">
     <Card sx={{ minWidth:50, maxHeight: 90 }}>
-        <CardContent>
-        <Typography sx={{ fontSize: 10, textAlign: 'center'  }} color="text.secondary" gutterBottom>
+        <CardContent sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+        {matches? <Typography sx={{ fontSize: 10, textAlign: 'center'  }} color="text.secondary" gutterBottom>
             INSTALLATIONS
-        </Typography>
+        </Typography>: <HardwareIcon sx={{color: 'brown'}}/>}
         <Typography sx={{textAlign: 'center'  }} color="text.secondary">
             5
         </Typography>
@@ -39,10 +47,10 @@ const Statuses = () => {
   </Grid>
   <Grid gridColumn="span 1">
     <Card sx={{ minWidth:50, maxHeight: 90 }}>
-        <CardContent>
-        <Typography sx={{ fontSize: 10, textAlign: 'center'  }} color="text.secondary" gutterBottom>
+        <CardContent sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+        {matches? <Typography sx={{ fontSize: 10, textAlign: 'center'  }} color="text.secondary" gutterBottom>
             VERIFICATIONS
-        </Typography>
+        </Typography>:<TaskIcon sx={{color: 'purple'}}/>}
         <Typography sx={{textAlign: 'center'  }} color="text.secondary">
             6
         </Typography>
@@ -52,11 +60,11 @@ const Statuses = () => {
   </Card>
   </Grid>
   <Grid gridColumn="span 1">
-    <Card sx={{ minWidth:50, maxHeight: 90 }}>
-        <CardContent>
-        <Typography sx={{ fontSize: 10, textAlign: 'center'  }} color="text.secondary" gutterBottom>
+    <Card sx={{ minWidth:50, maxHeight: 90}}>
+        <CardContent sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+       {matches? <Typography sx={{ fontSize: 10, textAlign: 'center'  }} color="text.secondary" gutterBottom>
             WAITING FOR PAYMENT
-        </Typography>
+        </Typography>: <PaidIcon sx={{verticalAlign: 'center', color: 'green'}}/>}
         <Typography sx={{textAlign: 'center'  }} color="text.secondary">
             3
         </Typography>
