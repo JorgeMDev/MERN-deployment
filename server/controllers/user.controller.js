6180eeeac398bcbaec34c4b7863713e7cfde94e3
Jorge Macbook <jorgemartinez1990@gmail.com>
2022-11-27 12:38:46 -0500

    server fix

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -57,7 +57,7 @@ module.exports.login = async(req, res)=>{
     console.log(`user token is: ${userToken}`)
     
     res
-        .cookie("usertoken", userToken, {httpOnly: true, sameSite: "None"})
+        .cookie("usertoken", userToken, {httpOnly: true})
         .json({ msg: "success!" });  
 
     console.log(req.cookies)
