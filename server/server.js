abd10f30ae010239d19b0e3431ac9c7b9c891f74
Jorge Macbook <jorgemartinez1990@gmail.com>
2022-11-27 11:34:49 -0500

    set header

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({ extended: true}));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.set('Access-Control-Allow-Credentials', true)
   next();
 })
 
