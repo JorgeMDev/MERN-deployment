8de9745d9ec79d6d8e63740eb694be5b4e364a84
Jorge Macbook <jorgemartinez1990@gmail.com>
2024-01-15 07:13:40 -0500

    test of cookie

--- client/src/components/Test.jsx
+++ client/src/components/Test.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
 const Test = () => {
+
+
+    const newCookie = (event) => {
+        event.preventDefault();
+        axios.get(process.env.REACT_APP_API_URL + "/api/cookie")
+        .then(res=> {
+          console.log(res)
+          })
+        .catch(err => {
+          console.log(err.response.data)
+         
+        },[])}
+    
+    
+
+
+
   return (
-    <div>Prueba de componente</div>
+    <div>Prueba de componente
+
+        <button onClick={newCookie}>Add new cookie</button>
+
+
+        
+    </div>
   )
 }
 
