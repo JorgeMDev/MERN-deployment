015d1282e838cd5e052c160234d51f0d764c77b4
Jorge Macbook <jorgemartinez1990@gmail.com>
2024-01-18 17:43:32 -0500

    failsafe

--- client/src/components/VerificationTable.jsx
+++ client/src/components/VerificationTable.jsx
@@ -132,7 +132,7 @@ const VerificationTable = (props) => {
             <TableRow key={i}>
               <TableCell>{moment(eachCust.dos).format('MMM DD, YY')}</TableCell>
               <TableCell>{eachCust.office}</TableCell>
-              <TableCell>{eachCust.user.firstName} {eachCust.user.lastName}</TableCell>
+              <TableCell>{eachCust.user.firstName ? eachCust.user.firstName : '' } {eachCust.user.lastName ? eachCust.user.lastName : '' }</TableCell>
               <TableCell>{eachCust.firstName} {eachCust.lastName}</TableCell>
               <TableCell>{eachCust.phone}</TableCell>
               {/* <TableCell>${eachCust.price}</TableCell>
