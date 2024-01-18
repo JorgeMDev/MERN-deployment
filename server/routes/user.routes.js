9bb565be85290062ee4891404713e1325529b426
Jorge Macbook <jorgemartinez1990@gmail.com>
2024-01-17 20:49:57 -0500

    auth route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -4,7 +4,7 @@ const {authenticate} = require('../configs/jwt.config')
 module.exports = app =>{
     app.get(`/api/allUsers`, authenticate, UserController.index)
     app.get(`/api/cookie`, UserController.cookie)
-    app.post(`/api/register`, UserController.register)
+    app.post(`/api/register`, authenticate, UserController.register)
     app.post(`/api/login`, UserController.login)
     app.get(`/api/logout`, UserController.logout)
     app.get(`/api/getUser`, authenticate, UserController.getUser)
